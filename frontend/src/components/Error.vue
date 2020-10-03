1852671dec0217b174bcd80b9372dfa631cf059d
Antonia Ziegler <97anzier@gmail.com>
2020-10-03 14:52:02 +0200

    error styling

--- frontend/src/components/Error.vue
+++ frontend/src/components/Error.vue
@@ -1,26 +1,29 @@
 <template>
-	<a-row type="flex" justify="center" class="error">
-		<a-col span="2"><a-icon type="close-circle" style="font-size: 30px; margin: 0 auto;" /> </a-col
-		><a-col span="21"> <p>Error message asdasdasdsadasdasdasdsa</p></a-col></a-row
-	>
+  <a-row type="flex" justify="center" class="error">
+    <a-col span="2"
+      ><a-icon type="close-circle" style="font-size: 24px;" /> </a-col
+    ><a-col span="20">
+      <p>Error message asdasdasdsadasdasdasdsa</p></a-col
+    ></a-row
+  >
 </template>
 
 <script>
-export default {}
+export default {};
 </script>
 
 <style scoped>
 .error {
-	color: white;
-	width: 40vw;
-	display: flex;
-	background-color: #ff5252;
-	align-items: center;
-	margin: 0 auto;
-	padding: 10px 0;
+  color: white;
+  width: 40vw;
+  display: flex;
+  background-color: #ff5252;
+  align-items: center;
+  margin: 0 auto 20px auto;
+  padding: 10px 0;
 }
 p {
-	margin: 0;
-	font-size: 18px;
+  margin: 0;
+  font-size: 14px;
 }
 </style>
