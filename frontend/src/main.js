d70e7f8b55c131f55264ceac27429c05a2a1ec9f
yordandev <jordan.lsx13@gmail.com>
2020-10-13 14:44:19 +0200

    fix

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,7 +3,6 @@ import App from './App.vue'
 import router from './router'
 import Antd from 'ant-design-vue'
 import 'ant-design-vue/dist/antd.css'
-import GAuth from 'vue-google-oauth2'
 
 Vue.config.productionTip = false
 
